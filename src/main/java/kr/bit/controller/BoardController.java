d69110f601090ced8975f864927afa23b9c4ece2
KitsuneSiho <achromatic.flame@gmail.com>
2024-05-20 11:22:51 +0900

    processed 5% - 기본 셋팅 및 SQL DB 매퍼

--- src/main/java/kr/bit/controller/BoardController.java
+++ src/main/java/kr/bit/controller/BoardController.java
@@ -1,4 +1,18 @@
 package kr.bit.controller;
 
+import kr.bit.entity.Board;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
 public class BoardController {
+    //jackson-databind(객체를 -> json으로 데이터포맷으로 반환
+    @RequestMapping("/boardList")
+    public @ResponseBody List<Board> boardList() {
+        List<Board> list = boardMapper.getLists();
+        return list; //json 데이터 형식으로 반환해서 리턴하겠다
+    }
 }
--- src/main/java/kr/bit/mapper/BoardMapper.java
+++ src/main/java/kr/bit/mapper/BoardMapper.java
@@ -1,4 +1,23 @@
 package kr.bit.mapper;
 
+import kr.bit.entity.Board;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
 public interface BoardMapper {
+    public List<Board> getLists();
+
+    public void boardInsert(Board vo);
+
+    public Board boardContent(int idx);
+
+    public void boardDelete(int idx);
+
+    public void boardUpdate(Board vo);
+
+    @Update("update bitboard set count=count+1 where idx=#{idx}")
+    public void boardCount(int idx);
 }
