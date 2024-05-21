28a01a40350118d0b47a7b147ed03dea4f53136f
KitsuneSiho <achromatic.flame@gmail.com>
2024-05-21 13:16:09 +0900

    Processed - 20% : User테이블과 user DATA 추가. 상단 네비바 만들고 메인컨트롤러에서 네비바 불러오게 구현

--- src/main/java/kr/bit/controller/BoardController.java
+++ src/main/java/kr/bit/controller/BoardController.java
@@ -12,50 +12,12 @@ import kr.bit.entity.Board;
 import kr.bit.mapper.BoardMapper;
 
 @Controller
-public class BoardController{	
-	
-	@Autowired
-	BoardMapper boardMapper;
-	
-	@RequestMapping("/")
+public class BoardController{
+	@RequestMapping("/MainBoard")
 	public String home() {
-		return "home"; 
+		return "board/main";
 	}
-	
-	@RequestMapping("/boardList")
-	public @ResponseBody List<Board> boardList(){
-		List<Board> list=boardMapper.getLists();
-		return list; 
-	}
-	
-	@RequestMapping("/boardInsert")
-	public @ResponseBody void boardInsert(Board vo){
-		boardMapper.boardInsert(vo);
-	}
-	
-	@RequestMapping("/boardDelete")
-	public @ResponseBody void boardDelete(@RequestParam("idx") int idx) {
-		boardMapper.boardDelete(idx);
-	}
-	
-	@RequestMapping("/boardUpdate")
-	public @ResponseBody void boardUpdate(Board vo) {
-		boardMapper.boardUpdate(vo);
 
-	}
-	
-	@RequestMapping("/boardContent")
-	public @ResponseBody Board boardContent(int idx) {
-		Board vo=boardMapper.boardContent(idx);
-		return vo;  //{idx:2,title:w,content:33....}
-	}
-	
-	@RequestMapping("/boardCount")
-	public @ResponseBody Board boardCount(int idx) {
-		boardMapper.boardCount(idx);
-		Board vo=boardMapper.boardContent(idx);
-		return vo;
-	}
 }
 
 
