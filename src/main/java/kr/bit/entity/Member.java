512990b456da9eba6df5c329303b10e5c9dd5878
KitsuneSiho <achromatic.flame@gmail.com>
2024-05-22 09:38:17 +0900

    Processed - 24% : 회원가입창 구현 및 아이디 체크기능 구현

--- src/main/java/kr/bit/entity/Member.java
+++ src/main/java/kr/bit/entity/Member.java
@@ -4,12 +4,15 @@ import lombok.Data;
 
 @Data
 public class Member {
-    private int memberIdx;
-    private String memberID;
-    private String memberPw;
-    private String memberName;
-    private int memberAge;
-    private String memberGender;
-    private String memberEmail;
-    private String memberProfile;
+	
+	private int memberIdx; 
+	private String memberID; 
+	private String memberPw;
+	private String memberName; 
+	private int memberAge;
+	private String memberGender; 
+	private String memberEmail;
+	private String memberProfile;
+	
+
 }
