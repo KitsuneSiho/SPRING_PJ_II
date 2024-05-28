1bc59499e64e078c18e280b5a167df81ccbb867e
KitsuneSiho <x.tensor.vector@gmail.com>
2024-05-28 19:23:49 +0900

    Processed - 100% : confirm final test

--- src/main/java/kr/bit/config/SecurityConfig.java
+++ src/main/java/kr/bit/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
